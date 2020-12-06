400422f01688a1c54acbc4f524ce959a0c09b2d5
Stjepan Hadjic <d4be4st@gmail.com>
2020-12-06 09:38:40 +0100

    Day 06

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,69 @@
 defmodule AdventOfCode.Day06 do
+  alias AdventOfCode.Utils
+
   def part1(args) do
+    args
+    |> Utils.parse_input()
+    |> parse
+    |> sum_any_group
   end
 
   def part2(args) do
+    args
+    |> Utils.parse_input()
+    |> parse
+    |> sum_all_group
+  end
+
+  def parse(lines) do
+    lines
+    |> Enum.reduce([%{}], fn line, acc ->
+      parse_line(line, acc)
+    end)
+  end
+
+  def parse_line(line, [head | tail] = acc) do
+    case line do
+      "" ->
+        [%{} | acc]
+
+      answers ->
+        head =
+          answers
+          |> String.graphemes()
+          |> Enum.reduce(head, fn c, current ->
+            current
+            |> Map.update(c, 1, &(&1 + 1))
+          end)
+          |> Map.update("people", 1, &(&1 + 1))
+
+        [head | tail]
+    end
+  end
+
+  def sum_any_group(groups) do
+    groups
+    |> Enum.reduce(0, fn group, sum ->
+      keys =
+        group
+        |> Map.keys()
+        |> Enum.count()
+
+      keys - 1 + sum
+    end)
+  end
+
+  def sum_all_group(groups) do
+    groups
+    |> Enum.reduce(0, fn group, sum ->
+      count = group["people"]
+
+      keys =
+        group
+        |> Enum.filter(fn {k, v} -> v == count && k != "people" end)
+        |> Enum.count
+
+      keys + sum
+    end)
   end
 end
--- test/advent_of_code/day_06_test.exs
+++ test/advent_of_code/day_06_test.exs
@@ -3,19 +3,43 @@ defmodule AdventOfCode.Day06Test do
 
   import AdventOfCode.Day06
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "test/support/input06.txt"
     result = part1(input)
 
-    assert result
+    assert result == 11
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "test/support/input06.txt"
     result = part2(input)
 
-    assert result
+    assert result == 6
+  end
+
+  test "parse 'abc'" do
+    line = "abc"
+
+    assert parse_line(line, [%{}]) == [%{"a" => 1, "b" => 1, "c" => 1, "people" => 1}]
+  end
+
+  test "parse 'a' 'a' 'a'" do
+    line = "a"
+    acc = parse_line(line, [%{}])
+    acc = parse_line(line, acc)
+
+    assert parse_line(line, acc) == [%{"a" => 3, "people" => 3}]
+  end
+
+  test "adding new head" do
+    acc = parse_line("a", [%{}])
+
+    assert parse_line("", acc) == [%{}, %{"a" => 1, "people" => 1}]
+  end
+
+  test "sum group" do
+    groups = [%{"a" => 1, "people" => 1}, %{"a" => 1, "b" => 3, "c" => 2, "people" => 3}]
+
+    assert sum_any_group(groups) == 4
   end
 end
