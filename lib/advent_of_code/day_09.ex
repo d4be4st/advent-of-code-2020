9222d7fc0649d4c62073ab8392e4dbbd6caab0ff
Stjepan Hadjic <d4be4st@gmail.com>
2020-12-10 06:30:36 +0100

    Day 09

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,7 +1,94 @@
 defmodule AdventOfCode.Day09 do
-  def part1(args) do
+  alias AdventOfCode.Utils
+  alias AdventOfCode.Utils.HashList
+
+  def part1({file, preamble_number}) do
+    file
+    |> Utils.parse_input
+    |> Stream.map(&String.to_integer/1)
+    |> Utils.to_hash_list
+    |> find_nonmatching_number(0, preamble_number)
   end
 
-  def part2(args) do
+  def part2({file, target_sum}) do
+    file
+    |> Utils.parse_input
+    |> Stream.map(&String.to_integer/1)
+    |> Utils.to_hash_list
+    |> find_contiguous(0, target_sum)
+  end
+
+  # part 1
+  def find_nonmatching_number(list, min, current) do
+    if list.count == current do
+      :stop
+    else
+      case check_preamble_numbers(list, min, current) do
+        {:next, number} ->  number
+        {:match, _number} -> find_nonmatching_number(list, min + 1, current + 1)
+      end
+    end
+  end
+
+  def check_preamble_numbers(list, min, max) do
+    number_to_check = HashList.get(list, max)
+
+    check_preamble_pair(list, min, min, max, number_to_check)
+  end
+
+  def check_preamble_pair(list, i, j, max, number_to_check) do
+    case check_number(list, i, j, number_to_check) do
+      :next -> 
+        {i, j} = if j + 1 == max, do: { i + 1, i + 2 }, else: {i, j + 1}
+        if i + 1 == max do
+          {:next, number_to_check}
+        else
+          check_preamble_pair(list, i, j, max, number_to_check)
+        end
+      :match -> {:match, number_to_check}
+    end
+  end
+
+  def check_number(list, i, j, number_to_check) do
+    cond do
+      HashList.get(list, i) == HashList.get(list, j) -> :next
+      HashList.get(list, i) + HashList.get(list, j) == number_to_check -> :match
+      true -> :next
+    end
+  end
+
+  # part 2
+  def find_contiguous(list, start_index, target_sum) do
+    if list.count == start_index do
+      :stop
+    else
+      case sum_contiguous(list, start_index, 0, target_sum) do
+        {:match, index} -> find_min_max(list, start_index, index)
+        :next -> find_contiguous(list, start_index + 1, target_sum)
+      end
+    end
+  end
+
+  # def find_contiguous_from_index(list, start_index, target_sum) do
+  #   case sum_contiguous(list, start_index, 0, target_sum) do
+  #     {:match, index} -> {:match, start_index, index}
+  #     :next -> find_contiguous_from_index(list, start_index + 1, target_sum)
+  #   end
+  # end
+
+  def sum_contiguous(list, index, sum, target_sum) do
+    sum = sum + HashList.get(list, index)
+    cond do
+      sum == target_sum -> {:match, index}
+      sum < target_sum -> sum_contiguous(list, index + 1, sum, target_sum)
+      sum > target_sum -> :next
+    end
+  end
+
+  def find_min_max(list, min, max) do
+    found_list = for i <- min..max, do: HashList.get(list, i)
+    min = Enum.min(found_list)
+    max = Enum.max(found_list)
+    min + max
   end
 end
--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -3,19 +3,63 @@ defmodule AdventOfCode.Day09Test do
 
   import AdventOfCode.Day09
 
-  @tag :skip
+  alias AdventOfCode.Utils.HashList
+
   test "part1" do
-    input = nil
+    input = {"test/support/input09.txt", 5}
     result = part1(input)
 
-    assert result
+    assert result == 127
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = {"test/support/input09.txt", 127}
     result = part2(input)
 
-    assert result
+    assert result == 62
+  end
+
+  test "find nonmatching number" do
+    list = %HashList{ list: %{0 => 1, 1 => 3, 2 => 1, 3 => 2, 4 => 4, 5 => 10}, count: 6 }
+    assert find_nonmatching_number(list, 0, 4) == 10
+
+    list = %HashList{ list: %{0 => 1, 1 => 3, 2 => 1, 3 => 2, 4 => 4, 5 => 6}, count: 6 }
+    assert find_nonmatching_number(list, 0, 4) == :stop
+  end
+
+  test "check preamble numbers" do
+    list = %HashList{ list: %{0 => 1, 1 => 3, 2 => 1, 3 => 2, 4 => 4} }
+    assert check_preamble_numbers(list, 0, 4) == {:match, 4}
+
+    list = %HashList{ list: %{0 => 1, 1 => 3, 2 => 1, 3 => 2, 4 => 10} }
+    assert check_preamble_numbers(list, 0, 4) == {:next, 10}
+  end
+
+  test "check preamble pair" do
+    list = %HashList{ list: %{0 => 1, 1 => 3, 2 => 1, 3 => 2, 4 => 4} }
+    assert check_preamble_pair(list, 0, 1, 4, 4) == {:match, 4}
+
+    list = %HashList{ list: %{0 => 1, 1 => 3, 2 => 1, 3 => 2, 4 => 10} }
+    assert check_preamble_pair(list, 0, 1, 4, 10) == {:next, 10}
+  end
+
+  test "check number" do
+    list = %HashList{ list: %{0 => 1, 1 => 3, 2 => 1, 3 => 2, 4 => 4} }
+    assert check_number(list, 0, 2, 4) == :next
+    assert check_number(list, 0, 3, 4) == :next
+    assert check_number(list, 0, 1, 4) == :match
+  end
+
+  test "find contiguous" do
+    list = %HashList{ list: %{0 => 1, 1 => 3, 2 => 1, 3 => 2, 4 => 4}, count: 5 }
+    assert find_contiguous(list, 0, 5) == 4
+  end
+
+  test "sum contiguous" do
+    list = %HashList{ list: %{0 => 1, 1 => 3, 2 => 1, 3 => 2, 4 => 4} }
+
+    assert sum_contiguous(list, 0, 0, 5) == :match
+    assert sum_contiguous(list, 0, 0, 2) == :next
+    assert sum_contiguous(list, 1, 0, 6) == :match
   end
 end
