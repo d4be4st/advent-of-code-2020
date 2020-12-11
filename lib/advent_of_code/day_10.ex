24777bba7341dd120f0bae9c1f0e9750080858ea
Stjepan Hadjic <d4be4st@gmail.com>
2020-12-11 14:42:25 +0100

    Day 10

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,90 @@
 defmodule AdventOfCode.Day10 do
+  alias AdventOfCode.Utils
+
   def part1(args) do
+    args
+    |> Utils.parse_input()
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.sort()
+    |> List.insert_at(0, 0)
+    |> find_differences(%{1 => 0, 3 => 0})
+    |> multiply
   end
 
   def part2(args) do
+    args
+    |> Utils.parse_input()
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.sort()
+    |> List.insert_at(0, 0)
+    |> find_subsets
+    |> sum
+  end
+
+  # part 1
+  def find_differences([first, second | []], output) do
+    output
+    |> Map.update!(difference(first, second), &(&1 + 1))
+    |> Map.update!(difference(second, second + 3), &(&1 + 1))
+  end
+
+  def find_differences([first, second | _] = list, output) do
+    output =
+      output
+      |> Map.update!(difference(first, second), &(&1 + 1))
+
+    [_ | rest] = list
+
+    find_differences(rest, output)
+  end
+
+  def difference(first, second) do
+    second - first
+  end
+
+  def multiply(%{1 => ones, 3 => threes}), do: ones * threes
+
+  # part 2
+
+  def find_subsets(list) do
+    Enum.chunk_while(list, [], &chunk_fun/2, &chunk_after/1)
+  end
+
+  def sum(subsets) do
+    Enum.reduce(subsets, 1, fn subset, acc ->
+      count = Enum.count(subset) - 2
+
+      cond do
+        count == 1 ->
+          acc * :math.pow(2, count)
+
+        count == 2 ->
+          acc * :math.pow(2, count)
+
+        count == 3 ->
+          acc * (:math.pow(2, count) - 1)
+          
+        count <= 0 ->
+          acc
+      end
+    end)
+  end
+
+  def chunk_fun(element, []) do
+    {:cont, [element]}
+  end
+
+  def chunk_fun(element, acc) do
+    [head | tail] = acc
+
+    if element - head == 1 do
+      {:cont, [element | acc]}
+    else
+      {:cont, acc, [element]}
+    end
+  end
+
+  def chunk_after(acc) do
+    {:cont, acc, []}
   end
 end
--- test/advent_of_code/day_10_test.exs
+++ test/advent_of_code/day_10_test.exs
@@ -3,19 +3,36 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "test/support/input10.txt"
     result = part1(input)
 
-    assert result
+    assert result == 220
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "test/support/input10.txt"
     result = part2(input)
 
-    assert result
+    assert result == 19208
+  end
+
+  test "find differences" do
+    list = [0, 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4] |> Enum.sort 
+
+    output = %{1 => 0, 3 => 0}
+    assert find_differences(list, output) == %{1 => 7, 3 => 5}
+  end
+
+  test "find subsets" do
+    list = [1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19]
+
+    assert find_subsets(list) == [[1], [7,6,5,4], [12,11,10],[16,15], [19]]
+  end
+
+  test "sum subsets" do
+    subsets = [[1], [7,6,5,4], [12,11,10],[16,15], [19]]
+
+    assert sum(subsets) == 8
   end
 end
