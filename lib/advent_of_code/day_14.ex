dcecf7b5aa9b050c58ae29af2e28e1a65dd16d5c
Stjepan Hadjic <d4be4st@gmail.com>
2020-12-15 19:22:05 +0100

    Day 14 part 1

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -1,7 +1,103 @@
 defmodule AdventOfCode.Day14 do
+  use Bitwise
+
+  alias AdventOfCode.Utils
+
   def part1(args) do
+    args
+    |> Utils.parse_input
+    |> Enum.reduce({%{}, {0, 0}}, &run_line1/2)
+    |> sum
   end
 
   def part2(args) do
+    args
+    |> Utils.parse_input
+    |> Enum.reduce({%{}, 0}, &run_line2/2)
+    |> sum
+  end
+
+  def run_line1(line, {memory, mask}) do
+    case String.slice(line, 0..2) do
+      "mas" -> 
+        {memory, set_mask1(line)}
+      "mem" ->
+        {set_memory1(line, memory, mask), mask}
+    end
+  end
+
+  def set_mask1(line) do
+    mask = 
+      line
+      |> String.split(" = ")
+      |> List.last
+
+    and_mask = String.replace(mask, "X", "1") |> String.to_integer(2)
+    or_mask = String.replace(mask, "X", "0") |> String.to_integer(2)
+
+    {and_mask, or_mask}
+  end
+
+  def set_memory1(line, memory, {and_mask, or_mask}) do
+    parsed = Regex.named_captures(~r/mem\[(?<address>\d+)\] = (?<number>\d+)/, line)
+    number = String.to_integer(parsed["number"])
+    new_number = (number ||| or_mask) &&& and_mask
+    Map.put(memory, parsed["address"], new_number)
+  end
+
+  def run_line2(line, {memory, mask}) do
+    case String.slice(line, 0..2) do
+      "mas" -> 
+        {memory, set_mask2(line)}
+      "mem" ->
+        {set_memory2(line, memory, mask), mask}
+    end
+  end
+
+  def set_mask2(line) do
+    mask = 
+      line
+      |> String.split(" = ")
+      |> List.last
+
+    or_mask = String.replace(mask, "X", "0") |> String.to_integer(2)
+
+    xcount =
+      mask
+      |> String.graphemes
+      |> Enum.count(fn c -> c == "X" end)
+
+    mask = String.replace(mask, "0", "1")
+
+    masks =
+      for i <- (0..xcount+1) do
+        Integer.to_string(i, 2)
+        |> String.pad_leading(xcount, "0")
+        |> String.graphemes
+        |> Enum.reduce(mask, fn c, acc ->
+          String.replace(acc, "X", c, global: false)
+        end)
+        |> String.to_integer(2)
+      end
+
+    {or_mask, masks}
+  end
+
+  def set_memory2(line, memory, {or_mask, masks}) do
+    parsed = Regex.named_captures(~r/mem\[(?<address>\d+)\] = (?<number>\d+)/, line)
+    number = String.to_integer(parsed["address"])
+
+    for mask <- masks do
+      IO.inspect(Integer.to_string(number, 2), label: "number")
+      IO.inspect(Integer.to_string(or_mask, 2), label: "or mask")
+      IO.inspect(Integer.to_string(mask, 2), label: "mask")
+      IO.inspect(Integer.to_string(number ||| or_mask, 2), label: "OR")
+      ((number ||| or_mask) &&& mask) |> Integer.to_string(2) |> IO.inspect label: "AND"
+    end
+    # Map.put(memory, parsed["address"], new_number)
+  end
+
+  def sum({memory, _mask}) do
+    Enum.reduce(memory, 0, fn {address, value}, acc -> acc + value end)
   end
 end
--- test/advent_of_code/day_14_test.exs
+++ test/advent_of_code/day_14_test.exs
@@ -3,12 +3,11 @@ defmodule AdventOfCode.Day14Test do
 
   import AdventOfCode.Day14
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "test/support/input14.txt"
     result = part1(input)
 
-    assert result
+    assert result == 165
   end
 
   @tag :skip
@@ -17,5 +16,30 @@ defmodule AdventOfCode.Day14Test do
     result = part2(input)
 
     assert result
+  end
+
+  test "set mask" do
+    input = "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+
+    assert set_mask1(input) == {68_719_476_733, 64}
+  end
+
+  test "set_memory" do
+    assert set_memory1("mem[8] = 11", %{}, {68_719_476_733, 64}) == %{"8" => 73}
+    assert set_memory1("mem[7] = 101", %{}, {68_719_476_733, 64}) == %{"7" => 101}
+    assert set_memory1("mem[8] = 0", %{}, {68_719_476_733, 64}) == %{"8" => 64}
+  end
+
+  test "set mask 2" do
+    input = "mask = 000000000000000000000000000000X1001X"
+
+    assert set_mask2(input) == {18, [68719476702, 68719476703, 68719476734, 68719476735]}
+  end
+
+  test "set memory 2" do
+    masks = {18, [68719476702, 68719476703, 68719476734, 68719476735]}
+    input = "mem[42] = 100"
+
+    assert set_memory2(input, %{}, masks) == %{}
   end
 end
